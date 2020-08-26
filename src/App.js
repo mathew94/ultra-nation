19c28fb83c4fbe3265716264f264b86c0cf7013b
mathew94 <mathewrodrick94@hotmail.com>
2020-08-27 01:38:12 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    fetch("https://restcountries.eu/rest/v2/all")
+    .then(res => res.json())
+    .then(data => setCountries(data))
+    .catch(error => console.log(error))
+  }, [])
+
   return (
     <div className="App">
+      <h1>Country loaded: {countries.length}</h1>
+      <ul>
+        {
+          countries.map(country => <li>{country.name}</li>)
+        }
+      </ul>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
